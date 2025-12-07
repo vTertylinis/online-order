e475597d0a51274cc04081cc7ee045b705320993
vTertylinis <v.tertylinis@gmail.com>
2025-12-07 22:25:30 +0200

    Auto-generated commit

--- chunk-M4LFMYQJ.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./chunk-ILOOE64D.js";function a(r,o="gr"){if(!r)return"";let t=n[r];return t&&t[o]||r}export{a};
---
+++ chunk-MACLXMYU.js
@@ -0,0 +1,1 @@
+import{a as n}from"./chunk-GYGOVUIW.js";function a(r,o="gr"){if(!r)return"";let t=n[r];return t&&t[o]||r}export{a};
