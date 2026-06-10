b115021a8526833583304048b6c27c21f26d4c32
vTertylinis <v.tertylinis@gmail.com>
2026-06-10 13:16:05 +0300

    Auto-generated commit

---
+++ chunk-5THPW7JZ.js
@@ -0,0 +1,1 @@
+var e={production:!1,googleMapsApiKey:"AIzaSyDhKTaP09AGWGYGDGbDvgWD_EBoL-X7-w0",backendUrl:"https://unepigrammatic-harshly-dario.ngrok-free.dev"};export{e as a};
--- chunk-MZH7DSIF.js
+++
@@ -1,1 +0,0 @@
-var o={production:!1,googleMapsApiKey:"AIzaSyDhKTaP09AGWGYGDGbDvgWD_EBoL-X7-w0"};export{o as a};
